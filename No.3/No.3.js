a021eaaf9bcdee56f80d951fbfc0ed3901695998
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2022-01-23 19:28:51 -0500

    Update No.3.js

--- No.3/No.3.js
+++ No.3/No.3.js
@@ -1,11 +1,7 @@
 let obj = {'SPANISH':SPANISH_FUDGE, 'FRENCH':FRENCH_FUDGE, 'ENGLISH':ENGLISH_FUDGE}
-if (recipe in obj) {
-    if (recipe == 'FRENCH') {
-        chocolate = 7;
-    }
-    fudge=obj[recipe]
-} else {
-    fudge = 1;
+if (recipe == 'FRENCH') {
+    chocolate = 7;
 }
+fudge = recipe in obj ? obj[recipe] : 1
 amt = base * fudge;
 sugar = 2 * bottom(amt) + top(amt) * 1.17;
